fa3ef41d81062cc840c520421196fa701b826f5a
tbrooke <tom.brooke@gmail.com>
2014-12-04 17:18:07 -0500

    updated staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,7 +1,7 @@
 require_relative "production"
 
 Mail.register_interceptor(
-  RecipientInterceptor.new(ENV.fetch("EMAIL_RECIPIENTS"))
+  RecipientInterceptor.new('tom.brooke@gmail.com')
 )
 
 Rails.application.configure do
