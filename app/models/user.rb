7d647ddb70f1d4c2e92b5ac665ed19d305af6014
tbrooke <tom.brooke@gmail.com>
2014-12-08 22:20:40 -0500

    added enum roles to user Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
+  enum role: [ :ogx_admin :admin, :user ]
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
