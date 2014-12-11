87e32da631d161de96a98883484d52ec1ca3c39d
tbrooke <tom.brooke@gmail.com>
2014-12-11 17:02:52 -0500

    removed default role assignemnt

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,9 @@ class User < ActiveRecord::Base
 
   after_initialize :set_default_role, :if => :new_record?
 
-  def set_default_role
-    self.role ||= :user
-  end
+  # def set_default_role
+  #   self.role ||= :user
+  # end
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
