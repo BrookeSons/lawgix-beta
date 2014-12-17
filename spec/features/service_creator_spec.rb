f02b6b4b33cc6dfe5959cee8adc76b18303138f0
tbrooke <tom.brooke@gmail.com>
2014-12-17 11:30:29 -0500

    moved service creator from actions to models

--- spec/features/service_creator_spec.rb
+++ spec/features/service_creator_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+ require 'rails_helper'
 
 
 RSpec.describe ServiceCreator do
@@ -6,11 +6,10 @@ RSpec.describe ServiceCreator do
 fixtures :all
 include Warden::Test::Helpers
 
-
   describe 'adding services' do
 
-  it "creates a service object given a lease number, State and county" do
-     creator = Service.new(lease_number: '12345', state: 'Ohio', county: 'Rowan')
+  it "creates a Service Creator object given a lease number, State and county" do
+     creator = ServiceCreator.new(lease_number: '12345', state: 'Ohio', county: 'Rowan')
      creator.build
      expect(creator.lease_number).to eq('12345')
      expect(creator.state).to eq('Ohio')
@@ -24,7 +23,6 @@ include Warden::Test::Helpers
     it 'allows a user to create a service' do
       visit new_service_path
       fill_in 'Lease number', with: "123456"
-      fill_in "Lessee last name", with: "Exxon"
       fill_in "County", with: "Rowan"
       fill_in "State", with: "Ohio"
       click_on("Create Service")
