ef19c5d2d2fce95eb5a56fe053fa39886318c18c
tbrooke <tom.brooke@gmail.com>
2015-01-06 11:18:55 -0500

    set up clients controller and client model

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,28 @@
+class ClientsController < ApplicationController
+
+  def new
+    @client = Client.new
+  end
+
+  def show
+    @company = Company.find(params[:id])
+    # authorize @company
+  end
+
+  def index
+    @companies = Company.all
+  end
+
+  def create
+    @company = Company.new(secure_params)
+    @company.save
+
+    redirect_to companies_path
+  end
+
+  private
+
+  def secure_params
+    params.require(:company).permit(:name, :billing_address,:billing_contact,:city, :state, :zip, :phone, :email)
+  end
+end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,12 @@
 class Client
+  include ActiveModel::Model
+
+  attr_accessor :first_name,
+               :last_name,
+               :name,  # Company
+               :phone,
+               :email,
+               :type,
+               :password,
+               :confirmed_password
 end
