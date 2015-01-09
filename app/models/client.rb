e280fc3e758c7a0cca5ad38036d9a6ff6d4db70d
tbrooke <tom.brooke@gmail.com>
2015-01-09 17:41:27 -0500

    misc update

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,14 +1,4 @@
-class Client
-  include ActiveModel::Model
-
-  attr_accessor :first_name,
-               :last_name,
-               :name,  # Company
-               :phone,
-               :email,
-               :type,
-               :password,
-               :confirmed_password
+class Client < User
 
 
 
--- app/models/lawyer.rb
+++ app/models/lawyer.rb
@@ -1,2 +1,2 @@
-class Lawyer
+class Lawyer < User
 end
