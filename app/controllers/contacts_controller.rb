57c54b04da49fcec2d45db0e821116b2b9ddd9d7
tbrooke <tom.brooke@gmail.com>
2015-01-11 08:18:06 -0500

    finished contacts db and tweaked new service styling

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,32 @@
 class ContactsController < ApplicationController
+
+
+  def new
+    @contact = Contact.new
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+    # authorize @company
+  end
+
+  def index
+    @contacts = Contact.all
+  end
+
+  def create
+    @contacts = Contact.new(secure_params)
+    @contacts.save
+
+    redirect_to contacts_path
+  end
+
+  private
+
+  def secure_params
+    params.require(:contact).permit(:name, :address,:city, :state, :zip, :phone, :email)
+  end
+
+
+
 end
