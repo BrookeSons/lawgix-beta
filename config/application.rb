dada8c7c7af24e193b70bee3ef320a5286f7b0d1
tbrooke <tom.brooke@gmail.com>
2015-03-02 22:02:45 -0500

    deploy test

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module Lawgix
     config.action_controller.action_on_unpermitted_parameters = :raise
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # --   all .rb files in that directory are automatically loaded.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
