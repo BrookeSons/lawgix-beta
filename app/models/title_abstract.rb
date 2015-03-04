ca5fa66a85654f96dfa1de9670ca298e0e186351
tbrooke <tom.brooke@gmail.com>
2015-03-04 11:08:34 -0500

    fixed second typo

--- app/models/title_abstract.rb
+++ app/models/title_abstract.rb
@@ -3,7 +3,7 @@ class TitleAbstract < ActiveRecord::Base
 
   has_and_belongs_to_many :deeds, through: :deeds_abstracts
   has_and_belongs_to_many :parcels, through: :parcels_abstracts
-  has_and_belongs_to_many :lawyers, through :users_abstracts
+  has_and_belongs_to_many :lawyers, through: :users_abstracts
   has_and_belongs_to_many :abstractors, through: :users_abstracts
   accepts_nested_attributes_for :deeds
   accepts_nested_attributes_for :parcels
