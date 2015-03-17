d4433634292294f80a6ae2a2f9d9eb785bd71675
tbrooke <tom.brooke@gmail.com>
2015-03-16 20:34:02 -0400

    made most of the fixes

--- app/uploaders/doc_uploader.rb
+++ app/uploaders/doc_uploader.rb
@@ -20,7 +20,7 @@ class DocUploader < CarrierWave::Uploader::Base
 
   def cache_dir
     "#{Rails.root}/tmp/uploads"
-    endd
+   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
